c5f67efc3c0c792152c8edab53ee333da53da561
Muhammad Nazar Abdillah <muhammad.nazar.abd20@gmail.com>
2023-09-20 14:38:00 +0800

    Update Admin

--- resources/views/pages/admin/role/edit.blade.php
+++ resources/views/pages/admin/role/edit.blade.php
@@ -22,16 +22,6 @@
 
     <div class="row justify-content-center">
         <div class="col-md-12">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
             <div class="card">
                 <form action="{{ route('role.update', $role->id) }}" method="POST">
                     @csrf
